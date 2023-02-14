afa4bdf498b9808b065ba7a14eee6c5bccd29d64
Paola Garcia <lizpaogarcia2@gmail.com>
2023-02-14 20:33:56 -0300

    comandos de mongoose y express

---
+++ models/jokes.models.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose"); //voy a usar la libreria mongoose
+
+const JokeSchema = new mongoose.Schema({
+    setup: {
+        type: String,
+        required:[true, "Se necesita el valor"],
+        minlength: [10, "Minimo 6 caracteres"]
+    },
+    punchline: {
+        type: String,
+        required:[true, "Se necesita el valor"],
+        minlength: [10, "Minimo 6 caracteres"]
+    }
+});
+
+const Jokes = mongoose.model("chistes", JokeSchema);//de mi db
+
+module.exports = Jokes; //para poder usar en otros archivos
